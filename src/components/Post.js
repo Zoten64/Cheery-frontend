7b5bd505508295a7be1a600e696cf46bea67304d
Zoten64 <forjavligratta@gmail.com>
2024-07-21 03:27:00 +0200

    Feat: Start working on post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import styles from "../styles/Post.module.css";
 
 const Post = () => {
   return (
-    <div>
-       
-    </div>
-  )
-}
+    <Container className={styles.PostContainer}>
+      <Row>
+        <h2 className={styles.Title}>Title</h2>
+      </Row>
+      <Row className={styles.Image}>
+        <img
+          src="https://gratisography.com/wp-content/uploads/2024/01/gratisography-cyber-kitty-1170x780.jpg"
+          alt="placeholder"
+        />
+      </Row>
+      <Row>
+        <p>placeholder content</p>
+      </Row>
+      <Row>
+        <p>
+          <a>#Tags</a> <a>#goes</a> <a>#here</a>
+        </p>
+      </Row>
+    </Container>
+  );
+};
 
-export default Post
+export default Post;
--- src/pages/posts/Posts.js
+++ src/pages/posts/Posts.js
@@ -1,9 +1,17 @@
 import React from "react";
 import PostFilterLinks from "../../components/PostFilterLinks";
+import Post from "../../components/Post";
+import styles from "../../styles/Posts.module.css";
 
 const Posts = () => {
   return (
-    <PostFilterLinks />
+    <>
+      <PostFilterLinks />
+      {/* Spacer elements makes sure posts aren't overlapped by navbars */}
+      <div className={styles.Spacer} ></div>
+      <Post />
+      <div className={styles.Spacer} ></div>
+    </>
   );
 };
 
