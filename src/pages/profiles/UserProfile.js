70bac264a201aa442f8442e9f0441a7d6a4eff5e
Zoten64 <forjavligratta@gmail.com>
2024-07-29 23:58:19 +0200

    Feat: Add profile page

--- src/pages/profiles/UserProfile.js
+++ src/pages/profiles/UserProfile.js
@@ -1,13 +1,53 @@
 import React from "react";
 import Avatar from "../../components/Avatar";
-import { Container } from "react-bootstrap";
+import { Col, Row, Container } from "react-bootstrap";
+import styles from "../../styles/UserProfile.module.css";
+import Post from "../../components/Post";
 
 const UserProfile = () => {
   return (
     <>
       <Container>
-        <Avatar />
+        <Row>
+          <Col>
+            <Avatar />
+          </Col>
+          <Col>
+            <h2>@coolsunflower</h2>
+            <p>they/them</p>
+            <p>This is a bio</p>
+          </Col>
+          <Col>
+            <div>
+              <span class="material-symbols-outlined">person_add</span>
+            </div>
+          </Col>
+        </Row>
       </Container>
+      <Container className={styles.PostBar}>
+        <Row>
+          <Col className={styles.Icons}>
+            <span class="material-symbols-outlined">view_comfy_alt</span>
+            <p className="d-none d-md-inline">Posts</p>
+          </Col>
+          <Col className={styles.Icons}>
+            <span class="material-symbols-outlined">favorite</span>
+            <p className="d-none d-md-inline">Cheers</p>
+          </Col>
+          <Col className={styles.Icons}>
+            <span className="material-symbols-outlined">chat_bubble</span>
+            <p className="d-none d-md-inline">Comments</p>
+          </Col>
+          <Col className={styles.Icons}>
+            <span className="material-symbols-outlined">autorenew</span>
+            <p className="d-none d-md-inline">Reposts</p>
+          </Col>
+        </Row>
+      </Container>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </>
   );
 };
